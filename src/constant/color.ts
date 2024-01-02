565ba35799d2d308bc5d8c39cab622a7b438aed4
ShinYoung-Kim <lukey0515@naver.com>
2024-01-02 23:58:41 +0900

    chore: color 상수 등록

---
+++ src/constant/color.ts
@@ -0,0 +1,13 @@
+export const COLOR = {
+  WF900: '#1A202C',
+  WF800: '#2D3648',
+  WF700: '#4A5468',
+  WF600: '#717D96',
+  WF500: '#A0ABC0',
+  WF400: '#CBD2E0',
+  WF300: '#E2E7F0',
+  WF200: '#EDF0F7',
+  WF100: '#F7F9FC',
+  WFWHITE: '#FFFFFF',
+  MAIN01: '#FF6D2F',
+};
