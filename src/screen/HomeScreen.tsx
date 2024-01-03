bc50d056a2e227dc825e470a4ebf1e0cd9d8635b
ShinYoung-Kim <lukey0515@naver.com>
2024-01-04 00:52:57 +0900

    feat: empty home screen 구성

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,9 +1,12 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import UploadButton from "../component/UploadButton";
 
 function HomeScreen() {
     return (
         <View>
-            Home
+            <Text>오늘의 4컷</Text>
+            <Text>오늘 하루를 표현할 수 있는 사진 4장을 업로드해주세요</Text>
+            <UploadButton />
         </View>
     );
 }
