30a15fac581c844e1d73aaeefefafcfffc3e9a45
ShinYoung-Kim <lukey0515@naver.com>
2024-02-19 23:55:22 +0900

    chore: babel 설정 파일에 절대 경로 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@asset': './src/asset',
+          '@component': './src/component',
+          '@constant': './src/constant',
+          '@context': './src/context',
+          '@hook': './src/hook',
+          '@screen': './src/screen',
+          '@service': './src/service',
+          '@style': './src/style',
+          '@util': './src/util'
+        }
+      }
+    ]
+  ]
 };
