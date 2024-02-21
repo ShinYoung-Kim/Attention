518dcce215e6d42504433444b0a2eee19b243fb8
ShinYoung-Kim <lukey0515@naver.com>
2024-02-21 17:46:55 +0900

    feat: 리액트 쿼리 배치

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import {COLOR} from '@constant/color';
 import HomeScreen from '@screen/HomeScreen';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
@@ -10,11 +12,16 @@ function App(): React.JSX.Element {
     SplashScreen.hide();
   }, []);
 
+  const queryClient = new QueryClient();
+
   return (
     <SafeAreaView style={styles.background}>
-      <RecoilRoot>
-        <HomeScreen />
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RecoilRoot>
+          <HomeScreen />
+        </RecoilRoot>
+      </QueryClientProvider>
     </SafeAreaView>
   );
 }
