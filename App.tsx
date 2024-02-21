3d934c89c4bc0d4385ec727de91b0cf599325c11
ShinYoung-Kim <lukey0515@naver.com>
2024-02-21 17:41:38 +0900

    feat: recoil root 추가

--- App.tsx
+++ App.tsx
@@ -1,15 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import {COLOR} from '@constant/color';
 import HomeScreen from '@screen/HomeScreen';
 import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import {RecoilRoot} from 'recoil';
 
 function App(): React.JSX.Element {
   useEffect(() => {
@@ -18,7 +12,9 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={styles.background}>
-      <HomeScreen />
+      <RecoilRoot>
+        <HomeScreen />
+      </RecoilRoot>
     </SafeAreaView>
   );
 }
