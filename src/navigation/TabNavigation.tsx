49a96b531796a932bd06553e5dc569656c806b12
ShinYoung-Kim <lukey0515@naver.com>
2024-03-12 18:45:13 +0900

    feat: tab navigation에 아이콘 추가

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,3 +1,5 @@
+import Icon from '@component/Icon';
+import {COLOR} from '@constant/color';
 import {Tab} from '@constant/tab';
 import AlbumScreen from '@screen/AlbumScreen';
 import FeedScreen from '@screen/FeedScreen';
@@ -7,11 +9,60 @@ import React from 'react';
 
 function TabNavigation() {
   return (
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Album" component={AlbumScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: COLOR.WF900,
+        },
+        tabBarActiveTintColor: COLOR.WFWHITE,
+        tabBarInactiveTintColor: COLOR.WF500,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            Icon({name: 'Home', fill: focused ? 'WFWHITE' : 'WF500', size: 24}),
+        }}
+      />
+      <Tab.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            Icon({
+              name: 'Grid',
+              fill: focused ? 'WFWHITE' : 'WF500',
+              size: 24,
+            }),
+        }}
+      />
+      <Tab.Screen
+        name="Album"
+        component={AlbumScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            Icon({
+              name: 'Folder',
+              fill: focused ? 'WFWHITE' : 'WF500',
+              size: 24,
+            }),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) =>
+            Icon({
+              name: 'User',
+              fill: focused ? 'WFWHITE' : 'WF500',
+              size: 24,
+            }),
+        }}
+      />
     </Tab.Navigator>
   );
 }
