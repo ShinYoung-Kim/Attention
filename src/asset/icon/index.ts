83d86ff9f537031da40b09d34f8b3a99769568b3
ShinYoung-Kim <lukey0515@naver.com>
2024-03-26 00:13:28 +0900

    chore: plus 아이콘 index 파일에 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -2,3 +2,4 @@ export {default as Home} from './icon-home.svg';
 export {default as Folder} from './icon-folder.svg';
 export {default as User} from './icon-user.svg';
 export {default as Grid} from './icon-grid.svg';
+export {default as Plus} from './icon-plus.svg';
